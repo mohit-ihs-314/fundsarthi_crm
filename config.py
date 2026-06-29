651f562fa3f4a3b9990efe4b55df1d2499eea2e4
Dell <mohit@investorhomesolutions.in>
2026-06-29 15:27:55 +0530

    test

--- config.py
+++ config.py
@@ -5,12 +5,12 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "super-secret-key")
 
     DB_USER = os.getenv("DB_USER")
-    DB_PASSWORD = quote_plus(os.getenv("DB_PASSWORD"))
+    DB_PASS = quote_plus(os.getenv("DB_PASSWORD"))
     DB_HOST = os.getenv("DB_HOST")
     DB_NAME = os.getenv("DB_NAME")
 
     SQLALCHEMY_DATABASE_URI = (
-        f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:3306/{DB_NAME}"
+        f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:3306/{DB_NAME}"
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
